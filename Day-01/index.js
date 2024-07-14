6b4939dfe7ad38f6622024b96dde31b37fd2fab0
DeepeshSuryavanshi <suryavanshideepesh04@gmail.com>
2024-07-14 13:39:14 +0530

    Adding Day 01 tasks and activitys

---
+++ Day-01/index.js
@@ -0,0 +1,36 @@
+// Actrivity 01: variable declaration
+
+// Task-01 Declare a variable using var ,assigne it a number and log the value to the console.
+var num = 100
+console.log(num);
+// Task-02 Declare a variable using let ,assigne it a string and log the value to the console.
+let str = 'string';
+console.log(str);
+
+
+// Actrivity 02: Constant declaration
+
+// Task-03 Declare a variable using const ,assigne it a boolean and log the value to the console.
+const bool = true
+console.log(bool);
+
+// Actrivity 03: Data types
+
+// Task 04: create variable of diffrent data types (number ,string, boolean, object, array)
+// and log each var type by using typeof operator.
+var Str = 'Deepesh code this';
+var Numb = 101;
+var Boole = true;
+var Obj = {
+    name:'Deepesh singh',
+    age:21,
+    Tech:'MERN',
+    language:'Java Scritp'
+}
+var Arr = [10,20,30,40,50]
+console.log(typeof(Str));
+console.log(typeof(Numb));
+console.log(typeof(Boole));
+console.log(typeof(Obj));
+console.log(typeof(Arr));
+
---
+++ Day-01/reassign.js
@@ -0,0 +1,15 @@
+// Activity 04: Reassigning Variable
+
+// Task 05: Declaring a variable using  let. assigne a initial value ,reassigne a new value
+// and log both value to the console
+
+let value = 100
+console.log(value)
+value = "Deepesh";
+console.log(value);
+
+// Task 06 : try reassigne a variable  declared with const and observe the error
+const data = "Deepesh"
+console.log(data);
+data = 100
+console.log(data);
