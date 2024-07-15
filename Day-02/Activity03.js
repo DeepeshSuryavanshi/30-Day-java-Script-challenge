aa6e9e92a63ba2930b5297396e55e10fc5a19066
DeepeshSuryavanshi <suryavanshideepesh04@gmail.com>
2024-07-15 18:56:34 +0530

    activity 03 complated

--- Day-02/Activity03.js
+++ Day-02/Activity03.js
@@ -10,8 +10,15 @@ let y = 20
     console.log("y is greter then x");
  }
 
- // Task 08 : write a program to compare two number using >= and <= and log the result in the console
+ // Task 09 : write a program to compare two number using >= and <= and log the result in the console
  let  a = 90
  let b = 101 
   console.log("result of comparing A >= B ",a>=b);
   console.log("result of comparing A <= B ",a<=b);
+
+// Task 10 : write a program to compare two number using == and === and log the result in the console
+let Str = '20'
+let num = 20
+console.log("Var 1:",Str , "\nvar 2:",num);
+console.log("Compare str === num: ",Str === num);
+console.log("Compare str == num: ",Str == num);
